f2fc9d0f9d23d4651162398b160ea8a4577ab94a
Arun Fung <arunfung@gmail.com>
2022-08-28 16:58:31 +0800

    var、dynamic区别

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "arunfung";
+  print(name);
+  
+  dynamic name1 = 1;
+  print(name1);
+
+  var name2;
+  name2 = "arun";
+  print(name2);
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+}
