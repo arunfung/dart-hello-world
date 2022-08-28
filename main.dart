24ace9a9354d39dea60a383ff4016a4ed017ea99
Arun Fung <arunfung@gmail.com>
2022-08-28 17:17:23 +0800

    const、final

--- main.dart
+++ main.dart
@@ -1,13 +1,28 @@
 void main() {
   String name = "arunfung";
   print(name);
-  
+
   dynamic name1 = 1;
   print(name1);
 
   var name2;
   name2 = "arun";
   print(name2);
+
+  const double = "arun"; // 初始化就需要被赋值
+  print(double);
+
+  final double1; // 初始化
+  double1 = "1234"; // 只允许被赋值一次
+  print(double1);
+
+  final ages = [10,20,30]; // 初始化
+  ages[0] = 1; // 只允许被赋值一次
+  print(ages);
+
+  final time = DateTime.now(); // 只允许被赋值一次
+  print(time);
+
   for (int i = 0; i < 5; i++) {
     print('hello ${i + 1}');
   }
